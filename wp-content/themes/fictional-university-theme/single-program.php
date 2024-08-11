ff2a13f17d83f89117a40a9de3345ef5d45b3986
Michał Rudy <michallrudy9@gmail.com>
2024-08-11 22:02:20 +0200

    Search logic that aware of relationships part two

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -12,7 +12,7 @@ while (have_posts()) {
       <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i> All Programs</a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
 
-    <div class="generic-content"><?php the_content(); ?></div>
+    <div class="generic-content"><?php the_field('main_body_content'); ?></div>
 
     <?php
     $relatedProfessors = new WP_Query(array(
@@ -42,7 +42,7 @@ while (have_posts()) {
             <span class="professor-card__name"><?php the_title(); ?></span>
           </a>
         </li>
-    <?php }
+      <?php }
       echo '</ul>';
     }
 
@@ -80,7 +80,21 @@ while (have_posts()) {
       }
     }
 
-    ?>
+    wp_reset_postdata();
+    $relatedCampuses = get_field('related_campus');
+
+    if ($relatedCampuses) {
+      echo '<hr class="section-break">';
+      echo '<h2 class="headline headline--medium">' . get_the_title() . ' is Available At These Campuses:</h2>';
+
+      echo '<ul class="min-list link-list">';
+      foreach ($relatedCampuses as $campus) {
+      ?> <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus) ?></a></li> <?php
+                                                                                                              }
+                                                                                                              echo '</ul>';
+                                                                                                            }
+
+                                                                                                                ?>
 
   </div>
 
