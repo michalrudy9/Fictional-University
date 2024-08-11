81df3978f6603f55395c2380bc772caf86f6d614
Michał Rudy <michallrudy9@gmail.com>
2024-08-11 10:00:05 +0200

    Open and close search overlay

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -12,8 +12,8 @@
       <div class="site-footer__col-two-three-group">
         <div class="site-footer__col-two">
           <h3 class="headline headline--small">Explore</h3>
-          <nav>
-            <ul class="nav-list min-list">
+          <nav class="nav-list">
+            <ul>
               <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
@@ -24,8 +24,8 @@
 
         <div class="site-footer__col-three">
           <h3 class="headline headline--small">Learn</h3>
-          <nav>
-            <ul class="nav-list min-list">
+          <nav class="nav-list">
+            <ul>
               <li><a href="#">Legal</a></li>
               <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
               <li><a href="#">Careers</a></li>
@@ -50,6 +50,16 @@
 
   </div>
 </footer>
+
+<div class="search-overlay">
+  <div class="search-overlay__top">
+    <div class="container">
+      <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+      <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+      <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+    </div>
+  </div>
+</div>
 
 <?php wp_footer(); ?>
 </body>
