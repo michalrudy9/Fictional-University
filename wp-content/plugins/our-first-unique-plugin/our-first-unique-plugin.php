6039f353b044371752e49c11c56b3104a3864b08
Michał Rudy <michallrudy9@gmail.com>
2024-08-12 15:48:49 +0200

    Settings api saving settings data

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -13,7 +13,23 @@ class WordCountAndTimePlugin
   function __construct()
   {
     add_action('admin_menu', array($this, 'adminPage'));
+    add_action('admin_init', array($this, 'settings'));
   }
+
+  function settings()
+  {
+    add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+    add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
+    register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+  }
+
+  function locationHTML()
+  { ?>
+    <select name="wcp_location">
+      <option value="0">Beginning of post</option>
+      <option value="1">End of post</option>
+    </select>
+  <?php }
 
   function adminPage()
   {
@@ -24,6 +40,13 @@ class WordCountAndTimePlugin
   { ?>
     <div class="wrap">
       <h1>Word Count Settings</h1>
+      <form action="options.php" method="POST">
+        <?php
+        settings_fields('wordcountplugin');
+        do_settings_sections('word-count-settings-page');
+        submit_button();
+        ?>
+      </form>
     </div>
 <?php }
 }
