acec2f67052ab08a8aae083dc3cbde58b77fd555
Michał Rudy <michallrudy9@gmail.com>
2024-08-13 19:21:46 +0200

    Add missing plugin meta data

--- wp-content/plugins/are-you-paying-attention/index.php
+++ wp-content/plugins/are-you-paying-attention/index.php
@@ -1,10 +1,11 @@
 <?php
 
 /*
-  Plugin Name: Brad&rsquo;s Boilerplate Block Plugin
-  Version: 1.0
+  Plugin Name: Are You Paying Attention Quiz
+  Description: Give your readers a multiple choice question.
+  Version 1.0
   Author: Brad
-  Author URI: https://github.com/LearnWebCode
+  Author URI: https://www.udemy.com/user/bardschiff
 */
 
 if (! defined('ABSPATH')) exit; // Exit if accessed directly
