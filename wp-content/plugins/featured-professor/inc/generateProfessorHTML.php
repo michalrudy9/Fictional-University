f53a1d1d539d137144eee1966fbcf041cd092027
Michał Rudy <michallrudy9@gmail.com>
2024-08-14 15:14:59 +0200

    Professor preview in editor

--- wp-content/plugins/featured-professor/inc/generateProfessorHTML.php
+++ wp-content/plugins/featured-professor/inc/generateProfessorHTML.php
@@ -19,7 +19,7 @@ function generateProfessorHTML($id)
         <?php
         $relatedPrograms = get_field('related_programs');
         if ($relatedPrograms) { ?>
-          <p>Name teaches:
+          <p><?php echo wp_strip_all_tags(get_the_title()) ?> teaches:
             <?php foreach ($relatedPrograms as $key => $program) {
               echo get_the_title($program);
               if ($key != array_key_last($relatedPrograms) && count($relatedPrograms) > 1) {
