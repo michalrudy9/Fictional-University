670670f330ed4de8e36dc43e399373ae025670df
Michał Rudy <michallrudy9@gmail.com>
2024-08-21 11:22:48 +0200

    What is context in the interactivity api

--- wp-content/plugins/interactivity-quiz/src/render.php
+++ wp-content/plugins/interactivity-quiz/src/render.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * PHP file to use when rendering the block type on the server to show on the front end.
  *
@@ -10,30 +11,10 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
-// Generate unique id for aria-controls.
-$unique_id = wp_unique_id( 'p-' );
+
 ?>
 
-<div
-	<?php echo get_block_wrapper_attributes(); ?>
-	data-wp-interactive="create-block"
-	<?php echo wp_interactivity_data_wp_context( array( 'isOpen' => false ) ); ?>
-	data-wp-watch="callbacks.logIsOpen"
->
-	<button
-		data-wp-on--click="actions.toggle"
-		data-wp-bind--aria-expanded="context.isOpen"
-		aria-controls="<?php echo esc_attr( $unique_id ); ?>"
-	>
-		<?php esc_html_e( 'Toggle', 'interactivity-quiz' ); ?>
-	</button>
-
-	<p
-		id="<?php echo esc_attr( $unique_id ); ?>"
-		data-wp-bind--hidden="!context.isOpen"
-	>
-		<?php
-			esc_html_e( 'Interactivity Quiz - hello from an interactive block!', 'interactivity-quiz' );
-		?>
-	</p>
+<div data-wp-interactive="create-block" data-wp-context='{"clickCount": 0}'>
+	<p>The button below has been clicked <span data-wp-text="context.clickCount"></span> times.</p>
+	<button data-wp-on--click="actions.buttonHandler">Click me</button>
 </div>
